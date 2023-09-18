fca85f64c7dd07c4c918483a68127f388f733165
Gabriel  Palmyro <gabrielpalmyro@Mac-mini-de-Gabriel.local>
2023-09-18 17:38:25 -0300

    b question

--- InterIF/InterIF-2023/primeira-fase/b.py
+++ InterIF/InterIF-2023/primeira-fase/b.py
@@ -17,22 +17,26 @@ for i in range(voltas):
     s = input().split()
     sigla = s[0]
     tempo = s[1]
-    for competidor in competidores:
+    for i, competidor in enumerate(competidores):
         if sigla == competidor[1]:
-            competidor[2].append(tempo)
+            competidor[2].append((tempo, i))
 
-# Remove as voltas inválidas
 for i in range(inv):
     s = input().split()
     sigla = s[0]
     tempo = s[1]
+    removeu = False
     for competidor in competidores:
-        if sigla == competidor[1] and tempo in competidor[2]:
-            competidor[2].remove(tempo)
+        if sigla == competidor[1]:
+            for t in range(len(competidor[2])):
+                if competidor[2][t][0] == tempo:
+                    competidor[2].pop(t)
+                    break
+        if removeu:
+            break
 
-# Ordena os competidores com base nos tempos
-competidores.sort(key=lambda x: (x[2], x[0]))
+# Ordena os competidores com base nos tempos mais cedo, com desempate pelo tempo mais cedo da primeira volta
+competidores.sort(key=lambda x: (x[2][0], x[2].index(x[2][0])))
 
-# Imprime a classificação
 for i, competidor in enumerate(competidores):
-    print(i + 1, competidor[0], competidor[2][0])
+    print(i + 1, competidor[0], competidor[2][0][0])
